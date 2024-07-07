45bdabb116ca51cec448b2f6b657edc4a3332eb0
sosolikova <veronika.sosolikova@gmail.com>
2024-07-07 22:25:47 +0200

    Set labels

--- scenes/main.gd
+++ scenes/main.gd
@@ -4,10 +4,12 @@ extends Node
 const DINO_START_POS := Vector2i(150, 485)
 const CAM_START_POS := Vector2i(576, 324)
 var score : int
+const SCORE_MODIFIER : int = 10
 var speed : float
 const START_SPEED : float = 10.0
 const MAX_SPEED : int = 25
 var screen_size : Vector2i
+var game_running : bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,25 +19,37 @@ func _ready():
 func new_game():
 	# Reset variables
 	score = 0
+	show_score()
+	game_running = false
 	# Reset the nodes
 	$Dino.position = DINO_START_POS
 	$Dino.velocity = Vector2i(0,0)
 	$Camera2D.position = CAM_START_POS
 	$Ground.position = Vector2i(0,0)
+	
+	# Reset hud
+	$HUD.get_node("StartLabel").show()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	speed = START_SPEED
-	
-	# Move dino and camera
-	$Dino.position.x += speed
-	$Camera2D.position.x += speed
-	
-	# Update score
-	score += speed
-	
-	# Update ground position
-	if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
-		$Ground.position.x += screen_size.x
+	if game_running:
+		speed = START_SPEED
 		
-	
+		# Move dino and camera
+		$Dino.position.x += speed
+		$Camera2D.position.x += speed
+		
+		# Update score
+		score += speed
+		show_score()
+		
+		# Update ground position
+		if $Camera2D.position.x - $Ground.position.x > screen_size.x * 1.5:
+			$Ground.position.x += screen_size.x
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			game_running = true
+			$HUD.get_node("StartLabel").hide()
+		
+func show_score():
+	$HUD.get_node("ScoreLabel").text = "SCORE: " + str(score / SCORE_MODIFIER)
